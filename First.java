f5e8bc49a0a06e1a6882d91fe2a61a140b782b75
L-Logue <logue_lillie@students.columbusstate.edu>
2025-01-16 14:53:45 -0500

    Create First.java

---
+++ First.java
@@ -0,0 +1,14 @@
+/**
+ * Write and code input about future careers.
+ * File: First.java
+ * Author: Lillie Logue
+ * Date: 1/16/2025
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Teacher");
+        System.out.println("Why: Using my knowlege in cyber to teach, and I enjoy working with students.");
+        System.out.println("Median salary: $53,000");
+    }
+}
