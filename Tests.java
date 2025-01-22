e8ece176f28e2a21f82044db542d376b838eef92
L-Logue <logue_lillie@students.columbusstate.edu>
2025-01-21 19:34:09 -0500

    Update First.java

---
+++ Tests.java
@@ -0,0 +1,18 @@
+/**
+ * Write and code input about 3 test score to find average
+ * File: Tests.java
+ * Author: Lillie Logue
+ * Date: 1/25/2025
+ */
+public class Tests {
+    public static void main(String[] args) {
+        double score1 = 52.6;
+        double score2 = 88.4;
+        double score3 = 76.8;
+        double avr = (score1 + score2 + score3) / 3;
+        System.out.println("Test score1: " + score1);
+        System.out.println("Test score2: " + score2);
+        System.out.println("Test score3: " + score3);
+        System.out.println("The average of the 3 test scores is: " + avr);
+    }
+}
