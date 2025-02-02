b652d6805d1b88a5364771a6b957140e740b45a3
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-02 16:33:50 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,5 +1,5 @@
 /**
- * Write and code input about the value Pet  object a object that should have a name
+ * Write and code input about the value Pet a object that should have a name
  * File: Pet.java
  * Author: Lillie Logue
  * Date: 1/31/2025
