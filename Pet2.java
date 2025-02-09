4e2fc1d69c2924d0fd9f0793ada671ed597a3cc2
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-09 14:44:49 -0500

    Update Pet2.java

--- Pet2.java
+++ Pet2.java
@@ -2,48 +2,34 @@ public class Pet {
     private int age;
     private String name;
     private String type;
-
-    // Default Constructor
     public Pet() {
         name = "Pet Name";
         type = "Animal";
         age = 1;
     }
-
-    // Parameterized Constructor
     public Pet(String newName, String newType, int newAge) {
         setName(newName);
         setType(newType);
         setAge(newAge);
     }
-
-    // Accessors (Getters)
     public int getAge() {
         return age;
     }
-
     public String getName() {
         return name;
     }
-
     public String getType() {
         return type;
     }
-
-    // Mutators (Setters)
     public void setAge(int newAge) {
         age = newAge;
     }
-
     public void setName(String newName) {
         name = newName;
     }
-
     public void setType(String newType) {
         type = newType;
     }
-
-    // Speak method based on pet type
     public String speak() {
         if (type.equalsIgnoreCase("dog")) {
             return "Woof";
@@ -53,8 +39,6 @@ public class Pet {
             return "Yowl";
         }
     }
-
-    // toString method to display pet information
     public String toString() {
         return "Pet Information:\n" +
                 "Type: " + type + "\n" +
