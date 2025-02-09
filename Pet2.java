fcf2b8966c3644114555aaf93c02ac3d108a078d
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-09 14:43:17 -0500

    Update Pet2.java

--- Pet2.java
+++ Pet2.java
@@ -1,32 +1,25 @@
-import java.util.Scanner;
-
 public class Pet {
+    private int age;
     private String name;
     private String type;
-    private int age;
 
+    // Default Constructor
     public Pet() {
         name = "Pet Name";
         type = "Animal";
         age = 1;
     }
 
+    // Parameterized Constructor
     public Pet(String newName, String newType, int newAge) {
         setName(newName);
         setType(newType);
         setAge(newAge);
     }
 
-    public void setName(String newName) {
-        name = newName;
-    }
-
-    public void setType(String newType) {
-        type = newType;
-    }
-
-    public void setAge(int newAge) {
-        age = newAge;
+    // Accessors (Getters)
+    public int getAge() {
+        return age;
     }
 
     public String getName() {
@@ -37,10 +30,20 @@ public class Pet {
         return type;
     }
 
-    public int getAge() {
-        return age;
+    // Mutators (Setters)
+    public void setAge(int newAge) {
+        age = newAge;
     }
 
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public void setType(String newType) {
+        type = newType;
+    }
+
+    // Speak method based on pet type
     public String speak() {
         if (type.equalsIgnoreCase("dog")) {
             return "Woof";
@@ -51,38 +54,12 @@ public class Pet {
         }
     }
 
+    // toString method to display pet information
     public String toString() {
         return "Pet Information:\n" +
-               "Type: " + type + "\n" +
-               "Name: " + name + "\n" +
-               "Sound: " + speak() + "\n" +
-               "Age:  " + age;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        Pet pet1 = new Pet();
-        System.out.println(pet1);
-        System.out.println();
-
-        Pet pet2 = new Pet("Buster", "Dog", 11);
-        System.out.println(pet2);
-        System.out.println();
-
-        System.out.println("Enter animal type:");
-        String userType = scanner.nextLine();
-
-        System.out.println("Enter animal name:");
-        String userName = scanner.nextLine();
-
-        System.out.println("Enter animal age:");
-        int userAge = scanner.nextInt();
-
-        Pet pet3 = new Pet(userName, userType, userAge);
-        System.out.println();
-        System.out.println(pet3);
-
-        scanner.close();
+                "Type: " + type + "\n" +
+                "Name: " + name + "\n" +
+                "Sound: " + speak() + "\n" +
+                "Age: " + age;
     }
 }
