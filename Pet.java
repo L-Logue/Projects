7f7a86196c9aee3eb12312ca4d3a7e8d199e7953
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-09 14:17:52 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,33 +1,33 @@
 /**
- * Write and code input about the value Pet a object that should have a name
+ * Write and code input about the value Pet  object a object that should have a name
  * File: Pet.java
  * Author: Lillie Logue
  * Date: 1/31/2025
  */
 public class Pet {
     private String name;
-
     public Pet() {
-        setName("Pet Name");
+        name = "Pet Name";
+    }
+    public Pet(String newName) {
+        name = newName;
     }
     public void setName(String newName) {
-        this.name = newName;
+        name = newName;
     }
     public String getName() {
         return name;
     }
     public String toString() {
-    return "Pet Information:\n" +
-           "Name: " + name;
+        return "Pet Information:\n" +
+                "Name: " + name;
     }
-
     public static void main(String[] args) {
         Pet pet1 = new Pet();
         System.out.println(pet1);
         System.out.println();
-
-        Pet pet2 = new Pet();
-        pet2.setName("Buster");
+        
+        Pet pet2 = new Pet("Buster");
         System.out.println(pet2);
     }
 }
