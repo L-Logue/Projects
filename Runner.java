5eddeaa68110f598f47c94dc1f36492beb6215ef
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-16 14:01:54 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -4,8 +4,10 @@
  * Date: 2/13/2025
  * Description: Main class to run the Tests program.
  */
-public class Runner {
-    public static void main(String[] args) {
+public class Runner
+    {
+    public static void main(String[] args)
+        {
         Tests test = new Tests();  
         test.getAverage();         
         System.out.println(test);  
