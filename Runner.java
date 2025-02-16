294369b94d9219f0ddd66ebf5839870d958a50fe
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-16 11:43:51 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,37 +1,14 @@
 /**
- * Write and code runner to go with file Pet.java
- * File: Runner.javaa
+ * File: Runner.java
  * Author: Lillie Logue
- * Date: 02/05/2025
+ * Date: 2/13/2025
+ * Description: Main class to run the Tests program.
  */
-import java.util.Scanner;
-
 public class Runner {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        Pet pet1 = new Pet();
-        System.out.println(pet1);
-        System.out.println();
-        
-        Pet pet2 = new Pet("Buster", "Dog", 11);
-        System.out.println(pet2);
-        System.out.println();
-        
-        System.out.println("Enter animal type:");
-        String userType = scanner.nextLine();
-
-        System.out.println("Enter animal name:");
-        String userName = scanner.nextLine();
-
-        System.out.println("Enter animal age:");
-        int userAge = scanner.nextInt();
-
-        
-        Pet pet3 = new Pet(userName, userType, userAge);
-        System.out.println();
-        System.out.println(pet3);
-
-        scanner.close();
+        Tests test = new Tests();  
+        test.getAverage();         
+        System.out.println(test);  
     }
 }
+
