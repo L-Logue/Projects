ade71c521bc05cb5b3c511e76e1f82362742d61b
L-Logue <logue_lillie@students.columbusstate.edu>
2025-02-22 17:08:24 -0500

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,44 @@
+public class Calc {
+    private double num1;
+    private double num2;
+
+    public Calc() {
+        this.num1 = 0;
+        this.num2 = 0;
+    }
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+    public double getNum1() {
+        return num1;
+    }
+    public double getNum2() {
+        return num2;
+    }
+    public double add() {
+        return num1 + num2;
+    }
+    public double subtract() {
+        return num1 - num2;
+    }
+    public double multiply() {
+        return num1 * num2;
+    }
+    public double divide() {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+            return Double.NaN;
+        }
+    }
+    @Override
+    public String toString() {
+        return "Displaying private data fields using toString():\n" +
+               "Num1: " + num1 + "\n" +
+               "Num2: " + num2;
+    }
+}
