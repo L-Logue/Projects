0c277d8d9ee5130c5a06be65833e9b1378d89fef
L-Logue <logue_lillie@students.columbusstate.edu>
2025-03-30 19:03:45 -0400

    Create Assessment.java

---
+++ Assessment.java
@@ -0,0 +1,86 @@
+/**
+ A class that holds a grade for assessment.
+ */
+public class Assessment {
+    private int score;  // Numeric score
+
+    /**
+     The setScore method sets the score field.
+     @param s The value to store in score.
+     */
+
+    public void setScore(int s)
+    {
+        this.score = s;
+    }
+
+    /**
+     The getScore method returns the score.
+     @return The value stored in the score field.
+     */
+
+    public int getScore()
+    {
+        return this.score;
+    }
+
+    /**
+     The getGrade method returns a letter grade
+     determined from the score field.
+     @return The letter grade.
+     */
+
+    public char getGrade()
+    {
+        char letterGrade;
+        //use switch statement to determine letter grade from score
+        switch (this.score / 10) {
+            case 10:
+            case 9:
+                letterGrade = 'A';
+                break;
+            case 8:
+                letterGrade = 'B';
+                break;
+            case 7:
+                letterGrade = 'C';
+                break;
+            case 6:
+                letterGrade = 'D';
+                break;
+            default:
+                letterGrade = 'F';
+                break;
+        }
+
+
+
+
+        return letterGrade;
+    }
+
+    public static class Exam extends Assessment {
+        private int numQuestions;
+        private int numMissed;
+        private double pointsEach;
+
+        public Exam(int questions, int missed) {
+            this.numQuestions = questions;
+            this.numMissed = missed;
+            this.pointsEach = 100.0 / questions;
+            setScore((int) Math.round((questions - missed) * pointsEach));
+        }
+
+        public double getPointsEach() {
+            return pointsEach;
+        }
+
+        @Override
+        public String toString() {
+            return "Exam [Total Questions: " + numQuestions + 
+                   ", Missed Questions: " + numMissed + 
+                   ", Score: " + getScore() + 
+                   ", Grade: " + getGrade() + "]";
+        }
+    }
+}
