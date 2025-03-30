9fa14ed24657e15eec2dbca74d12886d5fc04d07
L-Logue <logue_lillie@students.columbusstate.edu>
2025-03-30 18:59:22 -0400

    Update Assessment.java

--- Assessment.java
+++ Assessment.java
@@ -1,39 +1,36 @@
 /**
  A class that holds a grade for assessment.
  */
+Here is the updated `Assessment` class with no comments for the `Exam` part, as requested:
+
+```java
 public class Assessment {
     private int score;  // Numeric score
 
     /**
-     The setScore method sets the score field.
-     @param s The value to store in score.
+     * The setScore method sets the score field.
+     * @param s The value to store in score.
      */
-
-    public void setScore(int s)
-    {
+    public void setScore(int s) {
         this.score = s;
     }
 
     /**
-     The getScore method returns the score.
-     @return The value stored in the score field.
+     * The getScore method returns the score.
+     * @return The value stored in the score field.
      */
-
-    public int getScore()
-    {
+    public int getScore() {
         return this.score;
     }
 
     /**
-     The getGrade method returns a letter grade
-     determined from the score field.
-     @return The letter grade.
+     * The getGrade method returns a letter grade
+     * determined from the score field.
+     * @return The letter grade.
      */
-
-    public char getGrade()
-    {
+    public char getGrade() {
         char letterGrade;
-        //use switch statement to determine letter grade from score
+        // use switch statement to determine letter grade from score
         switch (this.score / 10) {
             case 10:
             case 9:
@@ -52,10 +49,31 @@ public class Assessment {
                 letterGrade = 'F';
                 break;
         }
-
-
-
-
         return letterGrade;
+    }
+
+    public static class Exam extends Assessment {
+        private int numQuestions;
+        private int numMissed;
+        private double pointsEach;
+
+        public Exam(int questions, int missed) {
+            this.numQuestions = questions;
+            this.numMissed = missed;
+            this.pointsEach = 100.0 / questions;
+            setScore((int) Math.round((questions - missed) * pointsEach));
+        }
+
+        public double getPointsEach() {
+            return pointsEach;
+        }
+
+        @Override
+        public String toString() {
+            return "Exam [Total Questions: " + numQuestions + 
+                   ", Missed Questions: " + numMissed + 
+                   ", Score: " + getScore() + 
+                   ", Grade: " + getGrade() + "]";
+        }
     }
 }
