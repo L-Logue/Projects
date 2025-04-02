208db57e9dffe900a015afa30147c7b72da7e053
L-Logue <logue_lillie@students.columbusstate.edu>
2025-04-02 05:38:14 -0400

    Update Assessment.java

--- Assessment.java
+++ Assessment.java
@@ -58,29 +58,4 @@ public class Assessment {
 
         return letterGrade;
     }
-
-    public static class Exam extends Assessment {
-        private int numQuestions;
-        private int numMissed;
-        private double pointsEach;
-
-        public Exam(int questions, int missed) {
-            this.numQuestions = questions;
-            this.numMissed = missed;
-            this.pointsEach = 100.0 / questions;
-            setScore((int) Math.round((questions - missed) * pointsEach));
-        }
-
-        public double getPointsEach() {
-            return pointsEach;
-        }
-
-        @Override
-        public String toString() {
-            return "Exam [Total Questions: " + numQuestions + 
-                   ", Missed Questions: " + numMissed + 
-                   ", Score: " + getScore() + 
-                   ", Grade: " + getGrade() + "]";
-        }
-    }
 }
